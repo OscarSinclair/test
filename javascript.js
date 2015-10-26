c75c013e9d163fa9a92a62b5811f0c958627951f
husein Murtada <husein Murtada>
2015-10-26 20:30:26 +0300

    my first commit

---
+++ javascript.js
@@ -0,0 +1,9 @@
+/*global console, alert*/
+// this is the start of the javascript
+
+
+function myFunc() {
+    var x = ["x1", "a2", "g3", "r4", "o5"];
+    var z = x.slice(0,-1);
+    document.getElementById("demo").innerHTML = z;
+}
